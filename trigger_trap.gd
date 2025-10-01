87d47c4a6bb3f088ecfdd68f94ecbfda44152f6a
leductuanvt1 <leductuan3404@gmail.com>
2025-10-01 14:35:12 +0700

    update

--- trigger_trap.gd
+++ trigger_trap.gd
@@ -1,11 +1,45 @@
-extends Node2D  # hoặc StaticBody2D/KinematicBody2D tùy bạn
+extends Area2D
 
-var move_up = false
-var speed = 200
+@export var move_distance: float = 2000.0
+@export var move_speed: float = 3
 
-func move_spikes_up():
-	move_up = true
+var start_position: Vector2
+var target_position: Vector2
+var has_triggered = false
 
-func _physics_process(delta):
-	if move_up:
-		position.y -= speed * delta  # Di chuyển lên
+@onready var trigger_area: Area2D = $Trigger\ trap   # <-- sửa đúng tên node trong scene
+
+func _ready():
+	start_position = global_position
+	target_position = start_position + Vector2(move_distance, 0)
+
+	# Kết nối signal
+	self.body_entered.connect(_on_player_touch_deadly)
+	trigger_area.body_entered.connect(_on_player_touch_trigger)
+
+	# Thêm vào group để Player có thể reset
+	add_to_group("resettable_traps")
+
+	print("DeadlyObject ready at: ", global_position)
+
+func _on_player_touch_deadly(body):
+	if body.name.begins_with("CharacterBody2D"):
+		print("Player touched deadly area - killing!")
+		if body.has_method("die"):
+			body.die()
+
+func _on_player_touch_trigger(body):
+	if body.name.begins_with("CharacterBody2D") and not has_triggered:
+		print("Player triggered movement!")
+		has_triggered = true
+		move_object()
+
+func move_object():
+	print("Moving object right...")
+	var tween = create_tween()
+	tween.tween_property(self, "global_position", target_position, move_speed)
+
+func reset_object():
+	print("Resetting trap to start position")
+	global_position = start_position
+	has_triggered = false
