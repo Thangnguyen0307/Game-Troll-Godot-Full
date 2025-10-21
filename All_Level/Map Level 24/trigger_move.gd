61413aa61bca48ae5ccb34eff3c81a9f93a04941
Nguyễn Gia Huy <huyngse184439@fpt.edu.vn>
2025-10-21 16:51:27 +0700

    .

---
+++ All_Level/Map Level 24/trigger_move.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+var triggered: bool = false
+
+func _ready() -> void:
+	if collision_shape_2d:
+			# Dùng call_deferred để đảm bảo nó được tắt sau khi mọi thứ đã sẵn sàng
+			collision_shape_2d.call_deferred("set_disabled", false)
+
+func _physics_process(delta: float) -> void:
+	if triggered:
+		collision_shape_2d.call_deferred("set_disabled", true
+		)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+			print("Spike activated!")
+			triggered = true
+		
+
+func reset_trap():
+	triggered = false
+	_ready()
